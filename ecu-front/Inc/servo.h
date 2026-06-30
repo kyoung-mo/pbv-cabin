8ee189debac0906a29cd38be0ed1921612bbff30
Hyeonjoo-Kim <87061803+KimHyeonj00@users.noreply.github.com>
2026-06-30 16:37:26 +0900

    Add files via upload

---
+++ ecu-front/Inc/servo.h
@@ -0,0 +1,11 @@
+#ifndef INC_SERVO_H_
+#define INC_SERVO_H_
+
+#include <stdint.h>
+
+/* TIM2 CH1 = PA0, TIM2 CH2 = PA1 */
+void Servo_Init(void);
+void Servo_SetAngle(uint8_t channel, uint8_t angle);
+void Servo_Stop(void);
+
+#endif /* INC_SERVO_H_ */
