d04a8f52ee43e087067efb9b42117cea0fe66e25
Hyeonjoo-Kim <87061803+KimHyeonj00@users.noreply.github.com>
2026-07-08 15:30:36 +0900

    Delete ecu-front/Inc directory

--- ecu-front/Inc/front_seat_app.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_FRONT_SEAT_APP_H_
-#define INC_FRONT_SEAT_APP_H_
-
-#include "main.h"
-
-void FrontSeatApp_Init(I2C_HandleTypeDef *hi2c);
-void FrontSeatApp_Process(void);
-
-#endif /* INC_FRONT_SEAT_APP_H_ */
